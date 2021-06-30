04270f03f2ea30416587ea5f45b45a442b1dd654
Aakash Shukla <shukla.aa@northeastern.edu>
2021-06-30 13:38:41 -0400

    Add project files.

---
+++ UnionFind/Helpers/QuickFindUF.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnionFind.Helpers
+{
+    
+    class QuickFindUF
+    {
+        private int[] id; // access to component id (site indexed)
+        private int count; // number of components
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="n">Size of array</param>
+        public QuickFindUF(int n)
+        {
+            id = new int[n];
+            count = n;
+            for (int i = 0; i < n; i++)
+            {
+                id[i] = i;
+            }
+        }
+        /// <summary>
+        /// Returns number of components
+        /// </summary>
+        /// <returns> total number of components </returns>
+        public int Count()
+        {
+            return count;
+        }
+
+        /// <summary>
+        /// Checks if 2 nodes are connected
+        /// </summary>
+        /// <param name="p">First Node</param>
+        /// <param name="q">Second Node</param>
+        /// <returns>CHecks if the 2 components provided are connected</returns>
+        public bool connected(int p, int q)
+        {
+            return (id[p] == id[q]);
+        }
+
+    }
+}
